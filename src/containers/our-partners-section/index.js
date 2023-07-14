3034112c74a8ef31974da467d4bc0c7890b65141
Joshwa-Shahbaz <joshwashahbaz2@gmail.com>
2023-07-13 23:55:30 -0700

    preparing our partners section for computer size

--- src/containers/our-partners-section/index.js
+++ src/containers/our-partners-section/index.js
@@ -2,11 +2,15 @@ import Wrapper from "@/component/Wrapper";
 import Image from "next/image";
 import React from "react";
 import Seven from "../../../public/7-Eleven.png";
+import Makro from "../../../public/Makro.png";
+import TrueCoffee from "../../../public/TrueCoffee.png";
+import TrueYou from "../../../public/TrueYou.png";
+import CpFreshmart from "../../../public/CpFreshmart.png";
 
 const OurPartners = () => {
   return (
     <Wrapper className="!p-0 h-[760px] overflow-hidden relative">
-      <div className="flex justify-center flex-row lg:justify-around">
+      <div className="flex justify-center flex-row lg:justify-between">
         <div class="flex flex-col items-center justify-center relative mt-0 lg:mt-[227px] ">
           <div className="absolute text-start">
             <h1 class=" text-[#68BAE3] text-[42px]">
@@ -37,8 +41,20 @@ const OurPartners = () => {
           </div>
         </div>
         <div className="">
-          <div className="absolute top-[124px]">
+          <div className="absolute top-[124px] left-[671px]">
             <Image src={Seven} width="100%" alt="seven-logo" />
+          </div>
+          <div className="absolute top-[432px] left-[671px]">
+            <Image src={Makro} width="100%" alt="seven-logo" />
+          </div>
+          <div className="absolute top-[287px] left-[832px]">
+            <Image src={TrueCoffee} width="100%" alt="seven-logo" />
+          </div>
+          <div className="absolute top-[212px] left-[1039px]">
+            <Image src={TrueYou} width="100%" alt="seven-logo" />
+          </div>
+          <div className="absolute top-[462px] left-[980px]">
+            <Image src={CpFreshmart} width="100%" alt="seven-logo" />
           </div>
         </div>
       </div>
