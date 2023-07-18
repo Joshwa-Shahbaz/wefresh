5d7f076480454cd56f62b9b672ec58114a34c7e2
Joshwa-Shahbaz <joshwashahbaz2@gmail.com>
2023-07-18 03:00:53 -0700

    added transsition to the buttons

--- src/component/button.js
+++ src/component/button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ className, children }) => {
   return (
     <button
-      className={`bg-[#FFD476] w-[100px] mx-[10px] h-[35px] lg:w-[140px] lg:mx-[20px] rounded bg-btn-bg text-[16px]   ${className}`}
+      className={`bg-[#FFD476] w-[100px] mx-[10px] h-[35px] lg:w-[140px] lg:mx-[20px] rounded bg-btn-bg text-[16px] hover:scale-105 transition-transform   ${className}`}
     >
       {children}
     </button>
